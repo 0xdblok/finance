c183508ed08ee61c3a4807dbde4ecc997caf5d6c
0xdblok <dblok0x@gmail.com>
2024-04-04 18:24:58 +0200

    ee

--- app/component/heros.js
+++ app/component/heros.js
@@ -7,24 +7,27 @@ import mint from "../asset/mint.png";
 import matters from "../asset/matters.png";
 export default function Heros() {
   return (
-    <div className="flex  pt-10 xl:px-32 px-10 h-screen ">
-      <div className="w-1/3">
+    <div className="flex h-screen pt-10 xl:px-32 px-10  ">
+      <div className="pt-">
         {" "}
-        <Image
-          className=""
-          src={hero}
-          alt="hero"
-          width={450}
-          height={450}
-        />{" "}
-        <div className="flex justify-center">
-          <a href="https://x.com/snappfinance/">
-            {" "}
-            <Image className="" src={x} alt="hero" width={34} height={34} />
-          </a>
+        <div className=" h-full items-end">
+          {" "}
+          <Image
+            className="h-[480px] w-[380px]"
+            src={hero}
+            alt="hero"
+            width={400}
+            height={400}
+          />{" "}
+          <div className="flex justify-center items-end">
+            <a href="https://x.com/snappfinance/">
+              {" "}
+              <Image className="" src={x} alt="hero" width={34} height={34} />
+            </a>
+          </div>
         </div>
       </div>
-      <div className="w-2/3">
+      <div className="flex    flex-col">
         <p className="text-5xl pt-16 ">Snapp Finance</p>
         <div className="flex pt-12 justify-center items-center gap-20">
           <Image className="" src={mint} alt="hero" width={240} height={240} />
@@ -33,18 +36,18 @@ export default function Heros() {
             projects
           </p>
         </div>{" "}
-        <div className="flex pt-12 justify-center items-center gap-6">
+        <div className="flex  justify-center pt-[70px] gap-6">
           <div className="pl-12  ">
             <Image
               className=""
               src={matters}
               alt="matter"
-              width={350}
-              height={350}
+              width={300}
+              height={300}
             />
           </div>
           <div>
-            <Image className="" src={zig} alt="zig" width={350} height={350} />
+            <Image className="" src={zig} alt="zig" width={300} height={300} />
           </div>
         </div>
       </div>
